9079d78f97ffcfb85bb41e30e6837c2c135ed719
e.zelenov <e.zelenov@altatec.ru>
2020-12-06 00:53:36 +0300

    fixes

---
+++ IntegrationTestCore/HttpContentEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace IntegrationTestCore
+{
+    public class HttpContentEqualityComparer : IEqualityComparer<HttpContent>
+    {
+        public bool Equals(HttpContent x, HttpContent y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+            if (ReferenceEquals(x, null))
+                return false;
+            if (ReferenceEquals(y, null))
+                return false;
+            if (x.GetType() != y.GetType())
+                return false;
+
+            var strX = x.ReadAsStringAsync().GetAwaiter().GetResult();
+            var strY = y.ReadAsStringAsync().GetAwaiter().GetResult();
+            return Equals(strX, strY);
+        }
+
+        public int GetHashCode(HttpContent obj)
+        {
+            var str = obj.ReadAsStringAsync().GetAwaiter().GetResult();
+            var hashCode = str.GetHashCode();
+            return hashCode;
+        }
+
+        public static IEqualityComparer<HttpContent> Default { get; } = new HttpContentEqualityComparer();
+    }
+}
---
+++ IntegrationTestCore/HttpResponseMessageEqualityComparer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace IntegrationTestCore
+{
+    public class HttpResponseMessageEqualityComparer : IEqualityComparer<HttpResponseMessage>
+    {
+        public bool CompareHeaders { get; set; } = true;
+        public bool CompareReasonPhrase { get; set; } = true;
+        public bool CompareStatusCode { get; set; } = true;
+        public bool CompareVersion { get; set; } = true;
+        public bool CompareContent { get; set; } = true;
+        public bool Equals(HttpResponseMessage x, HttpResponseMessage y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+            if (ReferenceEquals(x, null))
+                return false;
+            if (ReferenceEquals(y, null))
+                return false;
+            if (x.GetType() != y.GetType())
+                return false;
+
+            return (!CompareHeaders || Equals(x.Headers, y.Headers)) &&
+                (!CompareReasonPhrase || x.ReasonPhrase == y.ReasonPhrase) &&
+                (!CompareStatusCode || x.StatusCode == y.StatusCode) &&
+                (!CompareVersion || Equals(x.Version, y.Version)) &&
+                (!CompareContent || HttpContentEqualityComparer.Default.Equals(x.Content, y.Content));
+        }
+
+        public int GetHashCode(HttpResponseMessage obj)
+        {
+            unchecked
+            {
+                var hashCode = (obj.Content != null && CompareContent ? obj.Content.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (obj.Headers != null && CompareHeaders ? obj.Headers.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (obj.ReasonPhrase != null && CompareReasonPhrase ? obj.ReasonPhrase.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (CompareStatusCode ? (int)obj.StatusCode : 0);
+                hashCode = (hashCode * 397) ^ (obj.Version != null && CompareVersion ? obj.Version.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+        public static IEqualityComparer<HttpResponseMessage> Default { get; } = new HttpResponseMessageEqualityComparer
+        {
+            CompareHeaders = true,
+            CompareReasonPhrase = true,
+            CompareStatusCode = true,
+            CompareVersion = true,
+            CompareContent = true,
+        };
+        public static IEqualityComparer<HttpResponseMessage> StatusCodeAndContent { get; } = new HttpResponseMessageEqualityComparer
+        {
+            CompareHeaders = false,
+            CompareReasonPhrase = false,
+            CompareStatusCode = true,
+            CompareVersion = false,
+            CompareContent = true,
+        };
+        public static IEqualityComparer<HttpResponseMessage> StatusCode { get; } = new HttpResponseMessageEqualityComparer()
+        {
+            CompareHeaders = false,
+            CompareReasonPhrase = false,
+            CompareStatusCode = true,
+            CompareVersion = false,
+            CompareContent = false,
+        };
+
+    }
+}
